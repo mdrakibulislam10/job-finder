06948d81eb2431e3b14daea105dd938ce16e09a9
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-04-11 02:36:55 +0600

    create ActiveLink

---
+++ src/ActiveLink/ActiveLink.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const ActiveLink = ({ to, children }) => {
+    return (
+        <NavLink
+            to={to}
+            className={({ isActive }) =>
+                isActive
+                    ? "underline underline-offset-4 text-purple-600"
+                    : ""
+            }
+        >
+            {children}
+        </NavLink>
+    );
+};
+
+export default ActiveLink;
