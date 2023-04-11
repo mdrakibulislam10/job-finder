607c56878c8bd814d2af3eb0864f46b8106c06cb
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-04-12 04:52:03 +0600

    implement loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 import Footer from './components/Footer/Footer'
-import { Toaster } from 'react-hot-toast';
+import { Toaster, toast } from 'react-hot-toast';
 
 function App() {
 
+  const navigation = useNavigation();
+
   return (
     <>
+
+      {
+        navigation.state === "loading"
+          ? <span className='absolute top-32 md:top-40 text-lg left-1 md:left-2/4 -ms-12'>Loading...</span>
+          : ""
+      }
+
       <Outlet />
       <Footer />
 
